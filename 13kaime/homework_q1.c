39c6edbc3dae75de370aa30b182f6092356053f3
Takaharu Yaguchi <yaguchi@pearl.kobe-u.ac.jp>
2022-07-12 21:42:34 +0900

    modify 13kaime

--- 13kaime/homework_q1.c
+++ 13kaime/homework_q1.c
@@ -1,25 +1,25 @@
-// homework1.c
+// homework_q1.c
 #include <stdio.h>
 
-void matvec(A, v, w, n){
-  int i,j;
+void swap(v, w, n){
+  int i;
+  int u;
   
-  for(){
-       for(){
-           w[ ] = w [ ] + A[ ][ ]*v[ ]; 
-       }
-   }
+  for(i=0;i<n;i++){
+      u = w[ ];//wの第i成分を u という変数にコピー（バックアップ）しておく
+      w[ ] = v[ ];//vの第i成分を wの第i成分に書き込む
+      v[ ] = u;//vの第i成分を，u に保存しておいた w の第i成分に置き換える．
+  }
     
    return;
 }
 
 int main(void){
    int i,j;
-   int A   ={{1,2},{3,4}} ;//2x2行列を保存するための，2次元の配列を宣言せよ．
-   int v  ={5,6};//2次元のベクトルを保存するための，１次元の配列を宣言せよ．
-   int w ;//2次元のベクトルを保存するための，１次元の配列を宣言せよ．
-   matvec(A, v, w, 2);
+   int v  ={1,2};//2次元のベクトルを保存するための，１次元の配列を宣言せよ．
+   int w  ={3,4};//2次元のベクトルを保存するための，１次元の配列を宣言せよ．
+   matvec(v, w, 2);
+   printf("%d, %d\n", v[0], v[1]);
    printf("%d, %d\n", w[0], w[1]);
    return 0; 
 }
-
